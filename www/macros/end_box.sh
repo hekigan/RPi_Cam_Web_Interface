67fa16f7d5517b9212766bdbc344ee589a9f9fea
Laurent Blanes <laurent.blanes@gmail.com>
2019-09-04 01:20:47 +0900

    Update end_box.sh

--- www/macros/end_box.sh
+++ www/macros/end_box.sh
@@ -16,9 +16,16 @@ mylogfile=${BASEDIR}/scheduleLog.txt
 
 #Check if script already running
 NOW=`date +"-%Y/%m/%d %H:%M:%S-"`
+# TODAY=`date +"%Y-%m-%d"`
+
 if [ -f $mypidfile ]; then
-    echo "${NOW} Script already running..." >> ${mylogfile}
-    exit
+    # test if script died without cleaning up file
+    if test "`find ${mypidfile} -mmin +30`"; then
+        rm -f -- '$mypidfile'
+    else
+        echo "${NOW} Script already running..." >> ${mylogfile}
+        exit
+    fi
 fi
 
 #Remove PID file when exiting
@@ -29,22 +36,27 @@ shopt -s nullglob # allows you to change additional shell optional behavior
 
 for i in *.mp4;
     do name=`echo $i | cut -d'.' -f1`;
+    filedate=`echo $name | cut -d'_' -f1`;
     echo "Filename: ${name} | from ${i}";
     #ffmpeg -i $i -s 1280x720 -c:a copy $name.mp4.mp4;
 
+    # prepare new directory
+    mkdir -p  ../nextcloud/videos-mini/${filedate}
+
 # get original video size
     echo "# Details
-name: /mnt/MIRRORHDD/rpicam/videos/converted/${name}.mp4
+name: /mnt/MIRRORHDD/rpicam/videos/converted/${filedate}/${name}.mp4
 size: $(du -h ${name}.mp4 | cut -f1)
-duration: $(ffmpeg -i ${name}.mp4  2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,//)" >> "../nextcloud/videos-mini/${name}.md"
+duration: $(ffmpeg -i ${name}.mp4  2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,//)" >> "../nextcloud/videos-mini/${filedate}/${name}.md"
 
     # resize the video to a 320px width
-    mini="../nextcloud/videos-mini/${name}.mp4"
+    mini="../nextcloud/videos-mini/${filedate}/${name}.mp4"
     ffmpeg -i $i -vf scale="320:-1" $mini
 
     # create a thumbnail gallery from the video
-    ${MACRODIR}/thumbnails-generator.sh 20 3x7 800 $mini ${mini}.jpg >> ${mylogfile}
+    ${MACRODIR}/thumbnails-generator.sh 20 5x4 800 $mini ${mini}.jpg >> ${mylogfile}
 
     # move the file to "converted"
-    mv $i converted/$i
+    mkdir -p converted/${filedate}
+    mv $i converted/${filedate}/$i
 done
